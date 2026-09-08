4d28536da5746cbf525f30074d833497d65483ef
iutgeiitoulonE105 <touloniut@gmail.com>
2026-09-08 17:49:02 +0200

    avancement du 08/09/2026

---
+++ Embedded/Robot_mounzenze_dettori.X/main.c
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.c
+ * Author: E306-PC2
+ *
+ * Created on September 8, 2026, 1:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include "ChipConfig.h"
+#include "IO.h"
+#include "timer.h"
+
+int main (void){
+/***********************************************************************************************///Initialisation oscillateur
+InitOscillator();
+/***********************************************************************************************/// Configuration des input et output (IO)
+InitIO();
+
+InitTimer23();
+InitTimer1();
+
+LED_BLANCHE_1 = 1;
+LED_BLEUE_1 = 1;
+LED_ORANGE_1 = 1;
+LED_ROUGE_1 = 1;
+LED_VERTE_1 = 1;
+/***********************************************************************************************/
+// Boucle Principale
+/***********************************************************************************************/
+
+while(1)
+    {
+    /*LED_BLANCHE_1 = !LED_BLANCHE_1;
+    LED_BLEUE_1 = !LED_BLEUE_1;
+    LED_ORANGE_1 = ! LED_ORANGE_1 ;
+    LED_ROUGE_1 = ! LED_ROUGE_1 ; 
+    LED_VERTE_1 = ! LED_VERTE_1;*/
+    } // fin main
+}
